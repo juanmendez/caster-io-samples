069fbc912c13b7ebf2bbecd9a4a95cb4be658499
Juan Mendez <juan.mendez.3892@gmail.com>
2019-09-18 10:44:59 -0500

    gradle updates

--- GradlePluginIntermediate/app/src/main/java/info/adavis/gradleplugin/MainActivity.java
+++ GradlePluginIntermediate/app/src/main/java/info/adavis/gradleplugin/MainActivity.java
@@ -1,9 +1,9 @@
 package info.adavis.gradleplugin;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import info.adavis.gradlepluginint.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
